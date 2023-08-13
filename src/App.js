f4611f9e2eb458592d5613162678bdcd34b4c3f0
Sayak SenGupta <91112848+LoNeWoLf003@users.noreply.github.com>
2023-08-14 00:37:00 +0530

    details section added

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
     element: <Checkout />,
   },
   {
-    path: "/product-detail",
+    path: "/product-detail/:id",
     element: <ProductDetailPage />,
   },
 ]);
