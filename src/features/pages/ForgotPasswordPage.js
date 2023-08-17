0636cf74ac68d86f8ecd50837dce5915cb9b54e3
Sayak SenGupta <91112848+LoNeWoLf003@users.noreply.github.com>
2023-08-18 02:53:02 +0530

    logout

---
+++ src/features/pages/ForgotPasswordPage.js
@@ -0,0 +1,9 @@
+import ForgotPassword from "../auth/components/ForgotPassword";
+import Login from "../auth/components/Login";
+function ForgotPasswordPage() {
+    return ( <div>
+        <ForgotPassword></ForgotPassword>
+    </div>  );
+}
+
+export default ForgotPasswordPage;
